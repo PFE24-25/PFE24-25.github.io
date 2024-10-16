cf1dc8847d1d518b4a173bcc70a588debaf01ecd
jirayaSAMA <yoko.xana.zana@gmail.com>
2024-10-16 17:21:27 +0100

    v0.1

--- books.js
+++ books.js
@@ -5,19 +5,24 @@
 function createCard(book) {
     const card = document.createElement('div');
     card.classList.add('card');
-    card.innerHTML = `<div>
+    card.innerHTML = `  <div class="offer-img-container">
                             <a href="${book.pdf_url}" target="_blank">
                                 <img src=".${book.thumbnail_url}" alt="pfe icon" class="offer-img">
                             </a>
                         </div>
-                        <div>
+                        <div class="offer-info">
                             <div>
-                                <p class="company-name">${book.company}</p>
-                                <p class="tags"> <b>Tags:</b>${book.Tags}</p>
-                                <p class="deadline"><b>Deadline :</b>${book.deadline}</p>
+                                <p class="company-name ${(book.company=="")?"hidden":""}"><b>${book.company}</b></p>
+                                <p class="tags ${(book.Tags=="")?"hidden":""}"> <b>Tags:</b>${book.Tags}</p>
+                                <p class="deadline ${(book.deadline=="")?"hidden":""}"><b>Deadline :</b>${book.deadline}</p>
                             </div>
-                            <div><i class="fa-solid fa-download downloadURL" href="${book.pdf_url}"></i>
-                                <i class="fa-brands fa-linkedin linkedinURL" href="${book.linked_url}"></i>
+                            <div>
+                                <a href="${book.pdf_url}" target="_blank">
+                                    <i class="fa-solid fa-download downloadURL  ${(book.pdf_url=="")?"hidden":""}" href="${book.pdf_url}"></i>
+                                </a>
+                                <a href="${book.linked_url}" target="_blank">
+                                    <i class="fa-brands fa-linkedin linkedinURL ${(book.linked_url=="")?"hidden":""}" href="${book.linked_url}"></i>
+                                </a>
                             </div>
                         </div>`;
     document.getElementById('book-container').appendChild(card);
@@ -25,4 +30,40 @@ function createCard(book) {
 
 for (const book of books) {
     createCard(book);
+}
+
+setInterval(function () {
+    const background = document.getElementById('background');
+    //background follow mouse and update top and left
+    background.style.top = window.mouseY;
+        background.style.left = window.mouseX;
+
+}, 1);
+
+document.onmousemove = handleMouseMove;
+function handleMouseMove(event) {
+    const background = document.getElementById('background');
+    var eventDoc, doc, body;
+
+    event = event || window.event; // IE-ism
+
+    // If pageX/Y aren't available and clientX/Y are,
+    // calculate pageX/Y - logic taken from jQuery.
+    // (This is to support old IE)
+    if (event.pageX == null && event.clientX != null) {
+        eventDoc = (event.target && event.target.ownerDocument) || document;
+        doc = eventDoc.documentElement;
+        body = eventDoc.body;
+
+        event.pageX = event.clientX +
+          (doc && doc.scrollLeft || body && body.scrollLeft || 0) -
+          (doc && doc.clientLeft || body && body.clientLeft || 0);
+        event.pageY = event.clientY +
+          (doc && doc.scrollTop  || body && body.scrollTop  || 0) -
+          (doc && doc.clientTop  || body && body.clientTop  || 0 );
+    }
+
+    // Use event.pageX / event.pageY here
+    window.mouseY = event.pageY + 'px';
+    window.mouseX = event.pageX + 'px';
 }
--- pdfs.js
+++ pdfs.js
@@ -5,7 +5,7 @@ books = [
     {
         "company": "The Great Gatsby",
         "deadline": "F. Scott Fitzgerald",
-        "Tags": 1925,
+        "Tags": "1925",
         "thumbnail_url": "./assets/Capture d'écran 2024-10-16 015508.png",
         "pdf_url": "https://github.com/PFE24-25/PFE24-25.github.io/blob/main/assets/pdfs/Olindas.pdf",
         "linked_url": "https://www.linkedin.com/in/nesrin-brahim-048573223/"
@@ -13,7 +13,31 @@ books = [
     {
         "company": "The Great Gatsby00",
         "deadline": "F. Scott Fitzgerald00",
-        "Tags": 1925,
+        "Tags": "",
+        "thumbnail_url": "./assets/Capture d'écran 2024-10-16 015508.png",
+        "pdf_url": "https://github.com/PFE24-25/PFE24-25.github.io/blob/main/assets/pdfs/Olindas.pdf",
+        "linked_url": "https://www.linkedin.com/in/nesrin-brahim-048573223/"
+    },
+    {
+        "company": "The Great Gatsby00",
+        "deadline": "F. Scott Fitzgerald00",
+        "Tags": "1925",
+        "thumbnail_url": "./assets/Capture d'écran 2024-10-16 015508.png",
+        "pdf_url": "",
+        "linked_url": "https://www.linkedin.com/in/nesrin-brahim-048573223/"
+    },
+    {
+        "company": "The Great Gatsby00",
+        "deadline": "F. Scott Fitzgerald00",
+        "Tags": "1925",
+        "thumbnail_url": "./assets/Capture d'écran 2024-10-16 015508.png",
+        "pdf_url": "https://github.com/PFE24-25/PFE24-25.github.io/blob/main/assets/pdfs/Olindas.pdf",
+        "linked_url": "https://www.linkedin.com/in/nesrin-brahim-048573223/"
+    },
+    {
+        "company": "The Great Gatsby00",
+        "deadline": "F. Scott Fitzgerald00",
+        "Tags": "1925",
         "thumbnail_url": "./assets/Capture d'écran 2024-10-16 015508.png",
         "pdf_url": "https://github.com/PFE24-25/PFE24-25.github.io/blob/main/assets/pdfs/Olindas.pdf",
         "linked_url": "https://www.linkedin.com/in/nesrin-brahim-048573223/"
