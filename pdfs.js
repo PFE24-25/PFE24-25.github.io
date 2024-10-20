3e418168ae9ab6a14aa6d54f9a10b1e13727eafe
jirayaSAMA <yoko.xana.zana@gmail.com>
2024-10-20 19:12:25 +0100

    ahla ness

--- pdfs.js
+++ pdfs.js
@@ -60,20 +60,4 @@ books = [
         "linked_url": "https://www.linkedin.com/posts/salma-abid-96471a162_cybersaezcuritaez-analystessoc-pentesters-activity-7252989036716290050-WQ2C?utm_source=share&utm_medium=member_desktop"
     },
 
-
-{
-    "company": "momy",
-    "deadline": "deadddd",
-    "Tags": "tagggg",
-    "thumbnail_url": "/assets/images/1729446709052_momy.png",
-    "pdf_url": "/assets/pdfs/1729446709052_momy.pdf",
-    "linked_url": "www.google.com"
-},
-{
-    "company": "abouda batata",
-    "deadline": "timoun ou bomba",
-    "Tags": "tagdd",
-    "thumbnail_url": "/assets/images/1729447573370_aboudabatata.png",
-    "pdf_url": "/assets/pdfs/1729447573370_aboudabatata.pdf",
-    "linked_url": "www.google.com"
-},];
+];
