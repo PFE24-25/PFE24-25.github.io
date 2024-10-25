e33df55e3c46624b7db179201f4669228f7b3492
Nesrin Brahim <90508335+dog-rose@users.noreply.github.com>
2024-10-25 22:11:04 +0100

    Update pdfs.js

--- pdfs.js
+++ pdfs.js
@@ -67,7 +67,7 @@ books = [
                     "thumbnail_url": "/assets/images/talanFrance.png",
                         "pdf_url": "",
                             "linked_url": "https://www.linkedin.com/posts/sophie-thevenet-798468111_talan-stage-recrutement-activity-7254069979023646721-6VId?utm_source=share&utm_medium=member_desktop"
-    }
+    },
 
 {
     "company": "Tradrly ",
