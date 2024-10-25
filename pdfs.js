bcc604fcb345ef33a6696d127b6088a99ddb43e9
dog-rose <nessrina.brahim@gmail.com>
2024-10-25 21:45:21 +0100

    trying to figure out pics issues

--- pdfs.js
+++ pdfs.js
@@ -5,7 +5,7 @@ books = [
         "company": "Olindias",
         "deadline": "3 November 2024",
         "Tags": " AI",
-        "thumbnail_url": "./assets/images/Olindias.png",
+        "thumbnail_url": "/assets/images/Olindias.png",
         "pdf_url": "./assets/pdfs/Olindas.pdf",
         "linked_url": "https://www.linkedin.com/posts/olindias_end-of-study-internship-opportunity-activity-7251955753182224384-VTUB?utm_source=share&utm_medium=member_desktop"
     },
@@ -13,7 +13,7 @@ books = [
         "company": "Decade",
         "deadline": "",
         "Tags": " E-commerce",
-        "thumbnail_url": "./assets/images/Decade.png",
+        "thumbnail_url": "/assets/images/Decade.png",
         "pdf_url": "./assets/pdfs/Decade.pdf",
         "linked_url": "https://www.linkedin.com/posts/decade_decade-recrute-des-stagiaires-pfe-activity-7252286192841330689-0FNm?utm_source=share&utm_medium=member_desktop"
     },
@@ -21,7 +21,7 @@ books = [
         "company": "Harvest Group",
         "deadline": "",
         "Tags": " Data, Software Development",
-        "thumbnail_url": "./assets/images/HarvestGroup.png",
+        "thumbnail_url": "/assets/images/HarvestGroup.png",
         "pdf_url": "",
         "linked_url": "https://www.linkedin.com/posts/ramysouilem_pfe-internship-tunisia-activity-7246811104599752705-ermO?utm_source=share&utm_medium=member_desktop"
     },
@@ -29,7 +29,7 @@ books = [
         "company": "SYNETIS",
         "deadline": "",
         "Tags": " Cybersecurity",
-        "thumbnail_url": "./assets/images/SYNETIS.jpeg",
+        "thumbnail_url": "/assets/images/SYNETIS.jpeg",
         "pdf_url": "",
         "linked_url": "https://www.linkedin.com/posts/activity-7252312340052721667-vOMO?utm_source=share&utm_medium=member_desktop"
     },
@@ -38,7 +38,7 @@ books = [
         "company": "MANERGY",
         "deadline": "",
         "Tags": " Energétique, Environnementale",
-        "thumbnail_url": "./assets/images/MANERGY.png",
+        "thumbnail_url": "/assets/images/MANERGY.png",
         "pdf_url": "./assets/pdfs/MANERGY.pdf",
         "linked_url": "https://www.linkedin.com/posts/groupemanergy_recrutement-activity-7252599416124059649-WIhQ?utm_source=share&utm_medium=member_desktop"
     },
@@ -46,7 +46,7 @@ books = [
         "company": "Alteca",
         "deadline": "",
         "Tags": " Development Full Stack, Data, AI",
-        "thumbnail_url": "./assets/images/Alteca.png",
+        "thumbnail_url": "/assets/images/Alteca.png",
         "pdf_url": "./assets/pdfs/Alteca.pdf",
         "linked_url": "https://www.linkedin.com/posts/alteca_stages-digital-lyon-activity-7252944013035454464-5wm5?utm_source=share&utm_medium=member_desktop"
     },
@@ -55,17 +55,8 @@ books = [
         "company": "HumanOne",
         "deadline": "",
         "Tags": " CyberSecurity, Analyse SOC, Pentesting, GRC",
-        "thumbnail_url": "./assets/images/HumanOne.png",
+        "thumbnail_url": "/assets/images/HumanOne.png",
         "pdf_url": "",
         "linked_url": "https://www.linkedin.com/posts/salma-abid-96471a162_cybersaezcuritaez-analystessoc-pentesters-activity-7252989036716290050-WQ2C?utm_source=share&utm_medium=member_desktop"
     },
-
-
-{
-    "company": "acouna",
-    "deadline": "batata",
-    "Tags": "taggg",
-    "thumbnail_url": "/assets/images/1729448227430_acouna.png",
-    "pdf_url": "/assets/pdfs/1729448227430_acouna.pdf",
-    "linked_url": "www.google.com"
-},];
+];
